4af7c4c496412f2a2ee8a25c4bf0e00688af3380
JesusBerry <haquem@scp.systems>
2015-08-06 00:01:36 -0400

    First Comment Sab

--- sab.sh
+++ sab.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Check if the Sab Process is running
 ps -ax | grep  sabnzbdplus | grep -v grep > /dev/null
 
+# If it's not running, check the exit status
+# I set the exist status to 666 because that is not a real status
+# So if Sab crashed for any reason or is not running, it will start
 if [ $?  != 666 ]
 then
 
+#Instead of starting it in screen, I am storing it in a file for reference
  sabnzbdplus > /home/haquem/log/sab
 
 fi
