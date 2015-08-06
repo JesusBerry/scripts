a1a84d4dcf05db85751bc3c2597485f7dcc9e551
JesusBerry <haquem@scp.systems>
2015-08-06 00:17:40 -0400

    First Comment hdd

--- hdd.sh
+++ hdd.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
+# Create a file called hddsize that has the date and the status of your drive and email it to someone. 
+# Add it to cron to run this once a day to get a report via email 
 date > /home/haquem/log/hddsize | df -h >> /home/haquem/log/hddsize | less /home/haquem/log/hddsize | mail -s "Steam HDD Info" kingraj232@gmail.com
